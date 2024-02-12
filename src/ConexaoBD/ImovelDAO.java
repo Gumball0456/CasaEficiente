539cf578a5b567243c0ce5fec71e6b4bdff4845a
gumball <dariltonmario14@gmail.com>
2024-02-12 15:58:51 +0100

    Adicionar versao mais recente

---
+++ src/ConexaoBD/ImovelDAO.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ConexaoBD;
+
+import entidades.Imovel;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author gumball
+ */
+public class ImovelDAO {
+    public ArrayList<Imovel> obterImoveis(){
+        
+        return null;
+    }
+}
