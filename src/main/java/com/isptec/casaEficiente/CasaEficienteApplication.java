8bffc674e9e25db59e772801babc560caa5699b5
gumball <dariltonmario14@gmail.com>
2024-02-12 16:50:13 +0100

    Adicionar ficheiros de inicializacao para a versao web

---
+++ src/main/java/com/isptec/casaEficiente/CasaEficienteApplication.java
@@ -0,0 +1,13 @@
+package com.isptec.casaEficiente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaEficienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaEficienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isptec/casaEficiente/CasaEficienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isptec.casaEficiente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaEficienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
